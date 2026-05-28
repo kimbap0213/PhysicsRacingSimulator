77a134fd90f0b912b767f7f025b0b92b026afbb1
Kim Hyun <35618313+kimbap0213@users.noreply.github.com>
2026-05-28 14:09:43 +0900

    refactor: 인풋 로직 컨트롤러 구조로 분리

---
+++ Assets/Scripts/RacingSimulation/Core/Controllers/InputController.cs
@@ -0,0 +1,36 @@
+﻿using RacingSimulation.Runtime;
+using RacingSimulation.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RacingSimulation.Core.Controllers
+{
+    public class InputController
+    {
+        private readonly UnityEvent _onUpdate = new();
+        
+        public void Init(Vehicle vehicle)
+        {
+            _onUpdate.AddListener(() => vehicle.SetThrottleInput(InputUtil.GetVerticalInput(), Time.deltaTime));
+            _onUpdate.AddListener(() => vehicle.SetClutchInput(InputUtil.GetKeyInput(KeyCode.X), Time.deltaTime));
+            _onUpdate.AddListener(() => vehicle.SetSteeringInput(InputUtil.GetHorizontalInput(), Time.deltaTime));
+            _onUpdate.AddListener(() => vehicle.SetStarterInput(InputUtil.GetKeyInput(KeyCode.K)));
+            _onUpdate.AddListener(() =>
+            {
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    vehicle.GearShiftUp();
+                }
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    vehicle.GearShiftDown();
+                }
+            });
+        }
+        
+        public void UpdateInput()
+        {
+            _onUpdate.Invoke();
+        }
+    }
+}
